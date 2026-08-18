7d41c60236aa01bccf396dd16585bf71fc76a1ab
minhman <dk02012007@gmail.com>
2026-08-18 20:43:18 +0700

    chạy thử

--- cosolaptrinh/buoi01/baitap1.cs
+++ cosolaptrinh/buoi01/baitap1.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Channels;
 
 namespace cosolaptrinh.buoi01
 {
     internal class baitap1
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("chao cac ban!");
+            Console.WriteLine(30 + 6);
+        }
     }
 }
+
+        
+
+
